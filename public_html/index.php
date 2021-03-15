69248955d2bc52700a6cc3bf50c5405fac4ef538
frixels <67122965+frixels@users.noreply.github.com>
2021-03-15 10:59:40 +0200

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <?php
-    $Link = "https://whitelistme2.000webhostapp.com/";
+    $Link = "https://frixels.github.io/idkDEDtest/";
     $DirForImages = "iamges";
     $Tittle = "Example";
     $Description = "Example";
@@ -10,7 +10,7 @@
     $latest_file = key($files);
     echo $latest_file;
 
-    $s = '<html><head> <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" /><link type="application/json+oembed" href="https://whitelistme2.000webhostapp.com/doc.json" /><meta name="twitter:card" content="player" /> <meta name="twitter:site" content="@harry" /><meta name="twitter:title" content="' . $Tittle . '" /><meta name="twitter:description" content="' . $Description . '" /><meta name="twitter:player:width" content="320" /><meta name="twitter:player:height" content="180" /><meta name="twitter:player:stream:content_type" content="video/mp4" /> <meta property="og:image" content="' . $Link . $latest_file;
-    $x = '"/><meta name="twitter:card" content="summary_large_image"><meta name="theme-color" content="#0b68ea"/>';
+    $s = '<html><head> <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" /><link type="application/json+oembed" href="https://frixels.github.io/idkDEDtest/doc.json" /><meta name="twitter:card" content="player" /> <meta name="twitter:site" content="@harry" /><meta name="twitter:title" content="' . $Tittle . '" /><meta name="twitter:description" content="' . $Description . '" /><meta name="twitter:player:width" content="320" /><meta name="twitter:player:height" content="180" /><meta name="twitter:player:stream:content_type" content="video/mp4" /> <meta property="og:image" content="' . $Link . $latest_file;
+    $x = '"/><meta name="twitter:card" content="summary_large_image"><meta name="theme-color" content="#03fca1"/>';
     echo $s . $x;
 ?>
